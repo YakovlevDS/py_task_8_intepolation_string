1d380fa1498df6b6cf2e888a269cf115083aa65f
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 08:22:37 +0200

    feat:add solution create sales price

--- dict.py
+++ dict.py
@@ -9,38 +9,14 @@ users={
     'Juice':{'password':2222,'id':3},
 }
 
+sale_price={}
+for i in price:
+    sale_price[i]=round(price[i]*0.85, 2)
 
-# def buy():
-#     to_pay = 0
-#     while True:
-#         enter = input( 'What we buy???\n')
-#         if enter=='end':
-#             break
-#         to_pay += price[enter]
-#     return to_pay 
 
-# print(d1)    
-# print(d1['a']) 
-# d1['v'] =23 
-# print(d1) 
+print(price)
+print(sale_price)
 
-# del d1['v']
-# print(d1) 
-# print(d2) 
-# print(d3)
 
-# print(buy())
 
-# y= d1.get('a')
-# print(y)
-# t=d1.pop('a')
-# print(t)
-# print(d1)
-# d5=d1.copy()
-# print(d5)
-# print(d1.items())
-# print(d1.keys())
-# print(d1.values())
-# d1.update(d2)
-# print(d1)
 
