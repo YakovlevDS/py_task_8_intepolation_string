b34836706d3955ec4b78b8353b54885101687756
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 10:36:40 +0200

    feat:create copy binary file

--- saver.py
+++ saver.py
@@ -1,7 +1,7 @@
-# import os
+import os
 # list_paths=[]
 
-# for adress, folder, file in os.walk('C:\\'):
+# for adress, folder, file in os.walk('C:\\Users\Zver\Desktop'):
 #     for i in file:
 #         full_path = os.path.join(adress, i)
 #         list_paths.append(full_path)
@@ -17,5 +17,29 @@
 # print(u)
 # r.close
 
+# r = open('text.txt','w')
+# for x in list_paths:
+#     r.write(x + '\n')
+# r.close 
+#
+#     
+# r = open('text.txt')
+# for i in r:
+#     if 'text' in i:
+#         print(i)
+# r.close    
+
+r= open('e.exe','rb')
+y= open('Копия e.exe','wb')
+while True:
+    var = r.read(1048576) #1024*1024 bate read по 1 kB
+    print(var.__sizeof__()) # Show file size
+    if var.__sizeof__() == 33: 
+        break
+    y.write(var)   
+
+print('Контрол')
+r.close
+y.close
 
 
