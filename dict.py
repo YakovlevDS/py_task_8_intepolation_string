1e5605bb57bb8aa2c55c030f6465071906685caf
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 08:13:25 +0200

    feat:add dict methods

--- dict.py
+++ dict.py
@@ -1,5 +1,6 @@
+
 d1={'a':7}
-d2=dict(a=7)
+d2=dict(a=10)
 d3=dict.fromkeys([1,2,3,4,5,6,7,8,9,10],'val')
 price={'meat':3,'bread':1,'potato':10,'milk':7,'water':4,'oil':.1,'fish':0.5}
 users={
@@ -9,24 +10,37 @@ users={
 }
 
 
-def buy():
-    to_pay = 0
-    while True:
-        enter = input( 'What we buy???\n')
-        if enter=='end':
-            break
-        to_pay += price[enter]
-    return to_pay 
+# def buy():
+#     to_pay = 0
+#     while True:
+#         enter = input( 'What we buy???\n')
+#         if enter=='end':
+#             break
+#         to_pay += price[enter]
+#     return to_pay 
 
-print(d1)    
-print(d1['a']) 
-d1['v'] =23 
-print(d1) 
+# print(d1)    
+# print(d1['a']) 
+# d1['v'] =23 
+# print(d1) 
 
-del d1['v']
-print(d1) 
-print(d2) 
-print(d3)
+# del d1['v']
+# print(d1) 
+# print(d2) 
+# print(d3)
 
-print(buy())
+# print(buy())
+
+# y= d1.get('a')
+# print(y)
+# t=d1.pop('a')
+# print(t)
+# print(d1)
+# d5=d1.copy()
+# print(d5)
+# print(d1.items())
+# print(d1.keys())
+# print(d1.values())
+# d1.update(d2)
+# print(d1)
 
