4964156405d10dba17f4f75ddb32465ad0ea3ef0
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 09:39:45 +0200

    feat:create and write in file text

---
+++ saver.py
@@ -0,0 +1,16 @@
+# import os
+# list_paths=[]
+
+# for adress, folder, file in os.walk('C:\\'):
+#     for i in file:
+#         full_path = os.path.join(adress, i)
+#         list_paths.append(full_path)
+
+# Creates if it is not and writes to file text
+r=open('text.txt','w')
+r.write('string text')
+r.close
+
+
+
+
