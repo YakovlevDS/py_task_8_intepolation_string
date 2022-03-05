bfc98c78229daa62637aa407e3bbb1632a7870e8
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 16:50:40 +0200

    feat:add replace join splite

--- str.py
+++ str.py
@@ -9,15 +9,30 @@
 # print(k)
 # print(url)
 
-s='string text'
-print(s.upper())
-print(s.lower())
-print(s.capitalize())
+# s='string text'
+# print(s.upper())
+# print(s.lower())
+# print(s.capitalize())
 
 
-pathLin = 'C:/User/Py/Desktop/str.py'
-pathWin = pathLin.replace('/','\\') 
-print(pathWin)
+# pathLin = 'C:/User/Py/Desktop/str.py'
+# pathWin = pathLin.replace('/','\\') 
+# print(pathWin)
 
-r=pathWin.split('\\')
-print(r[-1]) # получение имени файла
+# r=pathWin.split('\\')
+# print(r[-1]) # получение имени файла
+
+
+q = open('text.txt', encoding='utf-8')
+r1 = (q.read())
+q.close()
+print(r1)
+list_simbol=['(',')','"',',','\n']
+for i in list_simbol:
+    r1 = r1.replace(i, '')
+print(r1)
+r2=r1.split()
+print(r2)
+
+new_text=' '.join(r2)
+print(new_text)
