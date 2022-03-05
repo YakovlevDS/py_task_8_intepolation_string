ddf851b0b41f4ae671023d9104ddb00ef2ab587f
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-05 16:20:26 +0200

    feat:add upper()

---
+++ str.py
@@ -0,0 +1,13 @@
+# s='Let\'s write a string as s'
+# d="Let\'s write a string 'as' s"
+# k="Let\'s write \n a string 'as' s"
+ # url="https:\\www.youtube.com\\new"
+# url=r"https:\\www.youtube.com\new"  #r --игнорировать 
+
+# print(s)
+# print(d)
+# print(k)
+# print(url)
+
+s='string text'
+print(s.upper())
